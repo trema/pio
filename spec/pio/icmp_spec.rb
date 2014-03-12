6a220fc70ce756d460a8d3b9138b21180015583b
Yasuhito Takamiya <yasuhito@gmail.com>
2014-03-12 14:17:42 +0900

    Refactori ICMP classes.

--- spec/pio/icmp_spec.rb
+++ spec/pio/icmp_spec.rb
@@ -3,10 +3,8 @@ require 'pio'
 
 describe Pio::Icmp do
   context '.read' do
-    subject { Pio::Icmp.read(data.pack('C*')) }
-
-    context 'with Icmp request frame' do
-      let(:data) do
+    context 'with an ICMP request frame' do
+      Given(:icmp_request_dump) do
         [
           # Destination MAC
           0x24, 0xdb, 0xac, 0x41, 0xe5, 0x5b,
@@ -51,103 +49,115 @@ describe Pio::Icmp do
           0x73, 0x74, 0x75, 0x76, 0x77, 0x61,
           0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
           0x68, 0x69
-        ]
+        ].pack('C*')
       end
 
-      its('destination_mac.to_s') { should eq '24:db:ac:41:e5:5b' }
-      its('source_mac.to_s') { should eq '00:26:82:eb:ea:d1' }
-      its(:ether_type) { should eq 0x0800 }
-      its(:ip_version) { should eq 0x4 }
-      its(:ip_header_length) { should eq 0x5 }
-      its(:ip_type_of_service) { should eq 0x0 }
-      its(:ip_total_length) { should eq 60 }
-      its(:ip_identifier) { should eq 0x39d3 }
-      its(:ip_fragment) { should eq 0 }
-      its(:ip_ttl) { should eq 128 }
-      its(:ip_protocol) { should eq 1 }
-      its(:ip_header_checksum) { should eq 0x2ed0 }
-      its('ip_source_address.to_s') { should eq '192.168.1.102' }
-      its('ip_destination_address.to_s') { should eq '8.8.8.8' }
-      its(:icmp_type) { should eq 8 }
-      its(:icmp_code) { should eq 0 }
-      its(:icmp_checksum) { should eq 0x4c5b }
-      its(:icmp_identifier) { should eq 0x0100 }
-      its(:icmp_sequence_number) { should eq 0x0001 }
-      its(:echo_data) { should eq 'abcdefghijklmnopqrstuvwabcdefghi' }
+      When(:icmp_request) do
+        Pio::Icmp.read(icmp_request_dump)
+      end
+
+      Then { icmp_request.class == Pio::Icmp::Request }
+      Then { icmp_request.destination_mac.to_s == '24:db:ac:41:e5:5b' }
+      Then { icmp_request.source_mac.to_s == '00:26:82:eb:ea:d1' }
+      Then { icmp_request.ether_type == 0x0800 }
+      Then { icmp_request.ip_version == 0x4 }
+      Then { icmp_request.ip_header_length == 0x5 }
+      Then { icmp_request.ip_type_of_service == 0x0 }
+      Then { icmp_request.ip_total_length == 60 }
+      Then { icmp_request.ip_identifier == 0x39d3 }
+      Then { icmp_request.ip_fragment == 0 }
+      Then { icmp_request.ip_ttl == 128 }
+      Then { icmp_request.ip_protocol == 1 }
+      Then { icmp_request.ip_header_checksum == 0x2ed0 }
+      Then { icmp_request.ip_source_address.to_s == '192.168.1.102' }
+      Then { icmp_request.ip_destination_address.to_s == '8.8.8.8' }
+      Then { icmp_request.icmp_type == 8 }
+      Then { icmp_request.icmp_code == 0 }
+      Then { icmp_request.icmp_checksum == 0x4c5b }
+      Then { icmp_request.icmp_identifier == 0x0100 }
+      Then { icmp_request.icmp_sequence_number == 0x0001 }
+      Then { icmp_request.echo_data == 'abcdefghijklmnopqrstuvwabcdefghi' }
     end
 
-    context 'with Icmp reply frame' do
-      let(:data) do
+    context 'with an ICMP reply frame' do
+      Given(:icmp_reply_dump) do
         [
-          # Destination MAC
-          0x00, 0x26, 0x82, 0xeb, 0xea, 0xd1,
-          # Source MAC
-          0x24, 0xdb, 0xac, 0x41, 0xe5, 0x5b,
-          # EtherType
-          0x08, 0x00,
-          # IP Version&IP Header Length
-          0x45,
-          # IP Type Of Service
-          0x00,
-          # IP Total Length
-          0x00, 0x3c,
-          # IP Identifier
-          0x00, 0x00,
-          # IP Flag&IP Fragment
-          0x00, 0x00,
-          # IP TTL
-          0x2d,
-          # IP Protocol
-          0x01,
-          # IP Header Checksum
-          0xbb, 0xa3,
-          # IP Source Address
-          0x08, 0x08, 0x08, 0x08,
-          # IP Destination Address
-          0xc0, 0xa8, 0x01, 0x66,
-          # ICMP Type
-          0x00,
-          # ICMP Code
-          0x00,
-          # ICMP Checksum
-          0x54, 0x5b,
-          # ICMP Identifier
-          0x01, 0x00,
-          # ICMP Sequence Number
-          0x00, 0x01,
-          0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a,
-          0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74,
-          0x75, 0x76, 0x77, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
-          0x68, 0x69
-        ]
+         # Destination MAC
+         0x00, 0x26, 0x82, 0xeb, 0xea, 0xd1,
+         # Source MAC
+         0x24, 0xdb, 0xac, 0x41, 0xe5, 0x5b,
+         # EtherType
+         0x08, 0x00,
+         # IP Version&IP Header Length
+         0x45,
+         # IP Type Of Service
+         0x00,
+         # IP Total Length
+         0x00, 0x3c,
+         # IP Identifier
+         0x00, 0x00,
+         # IP Flag&IP Fragment
+         0x00, 0x00,
+         # IP TTL
+         0x2d,
+         # IP Protocol
+         0x01,
+         # IP Header Checksum
+         0xbb, 0xa3,
+         # IP Source Address
+         0x08, 0x08, 0x08, 0x08,
+         # IP Destination Address
+         0xc0, 0xa8, 0x01, 0x66,
+         # ICMP Type
+         0x00,
+         # ICMP Code
+         0x00,
+         # ICMP Checksum
+         0x54, 0x5b,
+         # ICMP Identifier
+         0x01, 0x00,
+         # ICMP Sequence Number
+         0x00, 0x01,
+         0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a,
+         0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74,
+         0x75, 0x76, 0x77, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
+         0x68, 0x69
+        ].pack('C*')
       end
 
-      its('destination_mac.to_s') { should eq '00:26:82:eb:ea:d1' }
-      its('source_mac.to_s') { should eq '24:db:ac:41:e5:5b' }
-      its(:ether_type) { should eq 0x0800 }
-      its(:ip_version) { should eq 0x4 }
-      its(:ip_header_length) { should eq 0x5 }
-      its(:ip_type_of_service) { should eq 0 }
-      its(:ip_total_length) { should eq 60 }
-      its(:ip_identifier) { should eq 0 }
-      its(:ip_fragment) { should eq 0 }
-      its(:ip_ttl) { should eq 45 }
-      its(:ip_protocol) { should eq 1 }
-      its(:ip_header_checksum) { should eq 0xbba3 }
-      its('ip_source_address.to_s') { should eq '8.8.8.8' }
-      its('ip_destination_address.to_s') { should eq '192.168.1.102' }
-      its(:icmp_type) { should eq 0 }
-      its(:icmp_code) { should eq 0 }
-      its(:icmp_checksum) { should eq 0x545b }
-      its(:icmp_identifier) { should eq 0x0100 }
-      its(:icmp_sequence_number) { should eq 0x0001 }
-      its(:echo_data) { should eq 'abcdefghijklmnopqrstuvwabcdefghi' }
+      When(:icmp_reply) do
+        Pio::Icmp.read(icmp_reply_dump)
+      end
+
+      Then { icmp_reply.class == Pio::Icmp::Reply }
+      Then { icmp_reply.destination_mac.to_s == '00:26:82:eb:ea:d1' }
+      Then { icmp_reply.source_mac.to_s == '24:db:ac:41:e5:5b' }
+      Then { icmp_reply.ether_type == 0x0800 }
+      Then { icmp_reply.ip_version == 0x4 }
+      Then { icmp_reply.ip_header_length == 0x5 }
+      Then { icmp_reply.ip_type_of_service == 0 }
+      Then { icmp_reply.ip_total_length == 60 }
+      Then { icmp_reply.ip_identifier == 0 }
+      Then { icmp_reply.ip_fragment == 0 }
+      Then { icmp_reply.ip_ttl == 45 }
+      Then { icmp_reply.ip_protocol == 1 }
+      Then { icmp_reply.ip_header_checksum == 0xbba3 }
+      Then { icmp_reply.ip_source_address.to_s == '8.8.8.8' }
+      Then { icmp_reply.ip_destination_address.to_s == '192.168.1.102' }
+      Then { icmp_reply.icmp_type == 0 }
+      Then { icmp_reply.icmp_code == 0 }
+      Then { icmp_reply.icmp_checksum == 0x545b }
+      Then { icmp_reply.icmp_identifier == 0x0100 }
+      Then { icmp_reply.icmp_sequence_number == 0x0001 }
+      Then { icmp_reply.echo_data == 'abcdefghijklmnopqrstuvwabcdefghi' }
     end
 
-    context 'with an invalid Icmp frame' do
-      let(:data) { [] }
+    context 'with an invalid ICMP packet' do
+      When(:result) do
+        Pio::Icmp.read('')
+      end
 
-      it { expect { subject }.to raise_error(Pio::ParseError) }
+      Then { result == Failure(Pio::ParseError, 'End of file reached') }
     end
   end
 end
