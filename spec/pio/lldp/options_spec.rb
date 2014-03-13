804e99240fe03269a78428079722fec01414e40c
Yasuhito Takamiya <yasuhito@gmail.com>
2014-03-13 16:13:21 +0900

    Rewrite spec using rspec_given

--- spec/pio/lldp/options_spec.rb
+++ spec/pio/lldp/options_spec.rb
@@ -1,142 +1,186 @@
-# -*- coding: utf-8 -*-
 require 'pio'
 
-describe Pio::Lldp::Options do
-  context '.new' do
-    subject { Pio::Lldp::Options.new(options).to_hash }
+describe Pio::Lldp::Options, '.new' do
+  def options_with(user_options)
+    Pio::Lldp::Options.new(user_options)
+  end
 
-    context 'with :dpid and :port_number' do
-      let(:options) do
-        {
-         dpid: 0x192fa7b28d,
-         port_number: 1
-        }
+  Given(:mandatory_options) do
+    {
+      dpid: 0x192fa7b28d,
+      port_number: 1
+    }
+  end
+
+  context 'with :dpid and :port_number' do
+    Given(:options) { options_with(mandatory_options) }
+
+    describe '#to_hash' do
+      When(:result) { options.to_hash }
+
+      Then { result[:chassis_id] == 0x192fa7b28d }
+      Then { result[:port_id] == 1 }
+      Then do
+        result[:destination_mac] ==
+          Pio::Mac.new(Pio::Lldp::Options::DEFAULT_DESTINATION_MAC)
       end
-
-      its([:chassis_id]) { should eq 0x192fa7b28d }
-      its([:port_id]) { should eq 1 }
-      its([:destination_mac]) { should eq Pio::Mac.new('01:80:c2:00:00:0e') }
-      its([:source_mac]) { should eq Pio::Mac.new('01:02:03:04:05:06') }
-    end
-
-    context 'with :dpid and :port_number and :UNKNOWN_OPTION' do
-      let(:options) do
-        {
-          dpid: 0x192fa7b28d,
-          port_number: 1,
-          UNKNOWN_OPTION: :foo
-        }
-      end
-
-      it do
-        expect do
-          subject
-        end.to raise_error('Unknown option: UNKNOWN_OPTION.')
+      Then do
+        result[:source_mac] ==
+          Pio::Mac.new(Pio::Lldp::Options::DEFAULT_SOURCE_MAC)
       end
     end
 
-    context 'with :dpid, :port_number and :destination_mac' do
-      let(:options) do
-        {
-         dpid: 0x192fa7b28d,
-         port_number: 1,
-         destination_mac: '06:05:04:03:02:01'
-        }
+    context 'with :destination_mac' do
+      Given(:options) do
+        user_options =
+          mandatory_options.update(destination_mac: '06:05:04:03:02:01')
+        options_with(user_options)
       end
 
-      its([:chassis_id]) { should eq 0x192fa7b28d }
-      its([:port_id]) { should eq 1 }
-      its([:destination_mac]) { should eq Pio::Mac.new('06:05:04:03:02:01') }
-      its([:source_mac]) { should eq Pio::Mac.new('01:02:03:04:05:06') }
-    end
+      describe '#to_hash' do
+        When(:result) do
+          options.to_hash
+        end
 
-    context 'with :dpid, :port_number and :source_mac' do
-      let(:options) do
-        {
-         dpid: 0x192fa7b28d,
-         port_number: 1,
-         source_mac: '06:05:04:03:02:01'
-        }
-      end
-
-      its([:chassis_id]) { should eq 0x192fa7b28d }
-      its([:port_id]) { should eq 1 }
-      its([:destination_mac]) { should eq Pio::Mac.new('01:80:c2:00:00:0e') }
-      its([:source_mac]) { should eq Pio::Mac.new('06:05:04:03:02:01') }
-    end
-
-    context 'with :dpid, :port_number and :destination_mac(=nil)' do
-      let(:options) do
-        {
-         dpid: 0x192fa7b28d,
-         port_number: 1,
-         destination_mac: nil
-        }
-      end
-
-      its([:chassis_id]) { should eq 0x192fa7b28d }
-      its([:port_id]) { should eq 1 }
-      its([:destination_mac]) { should eq Pio::Mac.new('01:80:c2:00:00:0e') }
-      its([:source_mac]) { should eq Pio::Mac.new('01:02:03:04:05:06') }
-    end
-
-    context 'with :dpid, :port_number,'\
-            ' :destination_mac(=nil) and :source_mac(=nil)' do
-      let(:options) do
-        {
-         dpid: 0x192fa7b28d,
-         port_number: 1,
-         destination_mac: nil,
-         source_mac: nil
-        }
-      end
-
-      its([:chassis_id]) { should eq 0x192fa7b28d }
-      its([:port_id]) { should eq 1 }
-      its([:destination_mac]) { should eq Pio::Mac.new('01:80:c2:00:00:0e') }
-      its([:source_mac]) { should eq Pio::Mac.new('01:02:03:04:05:06') }
-    end
-
-    context 'when :dpid is not passed' do
-      let(:options) { { port_number: 1 } }
-
-      it do
-        expect { subject }.to raise_error('The dpid option should be passed.')
+        Then { result[:destination_mac] == Pio::Mac.new('06:05:04:03:02:01') }
+        And { result[:chassis_id] == 0x192fa7b28d }
+        And { result[:port_id] == 1 }
+        And do
+          result[:source_mac] ==
+            Pio::Mac.new(Pio::Lldp::Options::DEFAULT_SOURCE_MAC)
+        end
       end
     end
 
-    context 'when :dpid is nil' do
-      let(:options) { { dpid: nil, port_number: 1 } }
+    context 'with :source_mac' do
+      Given(:options) do
+        user_options =
+          mandatory_options.update(source_mac: '06:05:04:03:02:01')
+        options_with(user_options)
+      end
 
-      it do
-        expect { subject }.to raise_error("The dpid option shouldn't be nil.")
+      describe '#to_hash' do
+        When(:result) do
+          options.to_hash
+        end
+
+        Then { result[:source_mac] == Pio::Mac.new('06:05:04:03:02:01') }
+        And { result[:chassis_id] == 0x192fa7b28d }
+        And { result[:port_id] == 1 }
+        And do
+          result[:destination_mac] ==
+            Pio::Mac.new(Pio::Lldp::Options::DEFAULT_DESTINATION_MAC)
+        end
       end
     end
 
-    context 'when :port_number is not passed' do
-      let(:options) { { dpid: 0x192fa7b28d } }
+    context 'with :destination_mac = nil' do
+      Given(:options) do
+        user_options = mandatory_options.update(destination_mac: nil)
+        options_with(user_options)
+      end
 
-      it do
-        expect do
-          subject
-        end.to raise_error('The port_number option should be passed.')
+      describe '#to_hash' do
+        When(:result) do
+          options.to_hash
+        end
+
+        Then do
+          result[:destination_mac] ==
+            Pio::Mac.new(Pio::Lldp::Options::DEFAULT_DESTINATION_MAC)
+        end
+        And { result[:chassis_id] == 0x192fa7b28d }
+        And { result[:port_id] == 1 }
+        And do
+          result[:source_mac] ==
+            Pio::Mac.new(Pio::Lldp::Options::DEFAULT_SOURCE_MAC)
+        end
       end
     end
 
-    context 'when :port_number is nil' do
-      let(:options) { { dpid: 1, port_number: nil } }
+    context 'with :source_mac = nil' do
+      Given(:options) do
+        user_options = mandatory_options.update(source_mac: nil)
+        options_with(user_options)
+      end
 
-      it do
-        expect do
-          subject
-        end.to raise_error("The port_number option shouldn't be nil.")
+      describe '#to_hash' do
+        When(:result) do
+          options.to_hash
+        end
+
+        Then do
+          result[:source_mac] ==
+            Pio::Mac.new(Pio::Lldp::Options::DEFAULT_SOURCE_MAC)
+        end
+        And { result[:chassis_id] == 0x192fa7b28d }
+        And { result[:port_id] == 1 }
+        And do
+          result[:destination_mac] ==
+            Pio::Mac.new(Pio::Lldp::Options::DEFAULT_DESTINATION_MAC)
+        end
       end
     end
   end
-end
 
-### Local variables:
-### mode: Ruby
-### coding: utf-8-unix
-### indent-tabs-mode: nil
-### End:
+  context 'when :dpid is not passed' do
+    Given(:user_options) do
+      mandatory_options.delete(:dpid)
+      mandatory_options
+    end
+
+    When(:result) do
+      options_with(user_options)
+    end
+
+    Then do
+      result ==
+        Failure(ArgumentError, 'The dpid option should be passed.')
+    end
+  end
+
+  context 'with :dpid = nil' do
+    Given(:user_options) do
+      mandatory_options.update(dpid: nil)
+    end
+
+    When(:result) do
+      options_with(user_options)
+    end
+
+    Then do
+      result == Failure(ArgumentError, "The dpid option shouldn't be nil.")
+    end
+  end
+
+  context 'when :port_number is not passed' do
+    Given(:user_options) do
+      mandatory_options.delete(:port_number)
+      mandatory_options
+    end
+
+    When(:result) do
+      options_with(user_options)
+    end
+
+    Then do
+      result ==
+        Failure(ArgumentError, 'The port_number option should be passed.')
+    end
+  end
+
+  context 'with :port_number = nil' do
+    Given(:user_options) do
+      mandatory_options.update(port_number: nil)
+    end
+
+    When(:result) do
+      options_with(user_options)
+    end
+
+    Then do
+      result ==
+        Failure(ArgumentError, "The port_number option shouldn't be nil.")
+    end
+  end
+end
