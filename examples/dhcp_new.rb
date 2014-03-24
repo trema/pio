f6b6d34113ce9d74e2b295d88762937e8e57e2db
Yasuhito Takamiya <yasuhito@gmail.com>
2014-03-24 14:31:38 +0900

    Minimize the LOC of DHCP examples for readability.

--- examples/dhcp_new.rb
+++ examples/dhcp_new.rb
@@ -1,39 +1,30 @@
 require 'pio'
 
 discover = Pio::Dhcp::Discover.new(source_mac: '24:db:ac:41:e5:5b')
+discover.to_binary  # => DHCP Discover frame in binary format
 
 offer = Pio::Dhcp::Offer.new(
-  source_mac: '24:db:ac:41:e5:5b',
-  destination_mac: '11:22:33:44:55:66',
-  ip_source_address: '192.168.0.10',
+  source_mac: '00:26:82:eb:ea:d1',
+  destination_mac: '24:db:ac:41:e5:5b',
+  ip_source_address: '192.168.0.100',
   ip_destination_address: '192.168.0.1',
-  transaction_id: discover.transaction_id,
-  renewal_time_value_tlv: 0xdeadbeef,
-  rebinding_time_value_tlv: 0xdeadbeef,
-  ip_address_lease_time_tlv: 0xdeadbeef,
-  subnet_mask_tlv: '255.255.255.0'
+  transaction_id: discover.transaction_id
 )
+offer.to_binary  # => DHCP Offer frame in binary format
 
 request = Pio::Dhcp::Request.new(
   source_mac: '24:db:ac:41:e5:5b',
-  transaction_id: offer.transaction_id,
-  server_identifier_tlv: '192.168.0.10',
-  requested_ip_address_tlv: '192.168.0.1'
+  server_identifier_tlv: '192.168.0.100',
+  requested_ip_address_tlv: '192.168.0.1',
+  transaction_id: offer.transaction_id
 )
+request.to_binary  # => DHCP Request frame in binary format
 
 ack = Pio::Dhcp::Ack.new(
-  source_mac: '24:db:ac:41:e5:5b',
-  destination_mac: '11:22:33:44:55:66',
-  ip_source_address: '192.168.0.10',
+  source_mac: '00:26:82:eb:ea:d1',
+  destination_mac: '24:db:ac:41:e5:5b',
+  ip_source_address: '192.168.0.100',
   ip_destination_address: '192.168.0.1',
-  transaction_id: request.transaction_id,
-  renewal_time_value_tlv: 0xdeadbeef,
-  rebinding_time_value_tlv: 0xdeadbeef,
-  ip_address_lease_time_tlv: 0xdeadbeef,
-  subnet_mask_tlv: '255.255.255.0'
+  transaction_id: request.transaction_id
 )
-
-discover.to_binary  # => DHCP Discover frame in binary format
-offer.to_binary  # => DHCP Offer frame in binary format
-request.to_binary  # => DHCP Request frame in binary format
 ack.to_binary  # => DHCP Ack frame in binary format
