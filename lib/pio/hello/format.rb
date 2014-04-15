f125ae361e08ed001dfa04c646a4cccc6761306f
Yasuhito Takamiya <yasuhito@gmail.com>
2014-04-15 10:02:42 +0900

    Assert that hello message_type == 0.

--- lib/pio/hello/format.rb
+++ lib/pio/hello/format.rb
@@ -9,7 +9,7 @@ module Pio
       endian :big
 
       uint8 :version, value: 1
-      uint8 :message_type, value: 0
+      uint8 :message_type, initial_value: 0, assert: -> { value == 0 }
       uint16 :message_length, initial_value: 8
       uint32 :transaction_id
       string :body  # ignored
