d8fee16b91e83dd9e334f80f3036879373bd623d
Yasuhito Takamiya <yasuhito@gmail.com>
2014-04-15 16:51:38 +0900

    Add FeaturesReply and its test skeletons.

---
+++ lib/pio/features_reply.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+module Pio
+  # OpenFlow 1.0 Features Reply
+  class FeaturesReply
+  end
+end
---
+++ spec/pio/features_reply_spec.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+require 'pio'
+
+describe Pio::FeaturesReply do
+  describe '.read' do
+    pending 'Not yet implemented'
+  end
+
+  describe '.new' do
+    pending 'Not yet implemented'
+  end
+end
