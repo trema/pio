413c5475883aaa69d4c6ebcc0766ce71bf5e9cd0
Yasuhito Takamiya <yasuhito@gmail.com>
2014-12-12 11:11:50 +0900

    Fix port state.

--- lib/pio/open_flow/flags.rb
+++ lib/pio/open_flow/flags.rb
@@ -2,17 +2,27 @@
 
 # bitmap functions.
 module Flags
-  def flags(name, *flags)
+  # rubocop:disable MethodLength
+  def flags(name, *flags_array, **flags_hash)
+    flags = if flags_hash.empty?
+              flags_with_value = flags_array.map do |each|
+                [each, 1 << flags_array.index(each)]
+              end
+              Hash[flags_with_value]
+            else
+              flags_hash
+            end
     def_class_list flags
     def_field name
     def_get name, flags
     def_set name, flags
   end
+  # rubocop:enable MethodLength
 
   def def_class_list(flags)
     str = %(
       def self.list
-        #{flags.inspect}
+        #{flags.keys.inspect}
       end
     )
     module_eval str
@@ -22,22 +32,13 @@ module Flags
     module_eval("uint32 :#{name}")
   end
 
-  # rubocop:disable MethodLength
   def def_get(name, list)
     str = %{
       def get
         list = #{list.inspect}
-        case list
-        when Hash
-          list.each_with_object([]) do |(key, value), result|
-            result << key if #{name} & value != 0
-            result
-          end
-        when Array
-          (0..(list.length - 1)).each_with_object([]) do |each, result|
-            result << list[each] if #{name} & (1 << each) != 0
-            result
-          end
+        list.each_with_object([]) do |(key, value), result|
+          result << key if #{name} & value != 0
+          result
         end
       end
     }
@@ -48,21 +49,12 @@ module Flags
     str = %{
       def set(v)
         list = #{list.inspect}
-        case list
-        when Hash
-          v.each do |each|
-            fail "Invalid state flag: \#{v}" unless list.keys.include?(each)
-          end
-          self.#{name} = v.map { |each| list[each] }.inject(:|)
-        when Array
-          v.each do |each|
-            fail "Invalid #{name} flag: \#{v}" unless list.include?(each)
-          end
-          self.#{name} = v.map { |each| 1 << list.index(each) }.inject(:|)
+        v.each do |each|
+          fail "Invalid state flag: \#{v}" unless list.keys.include?(each)
         end
+        self.#{name} = v.map { |each| list[each] }.inject(:|)
       end
     }
     module_eval str
   end
-  # rubocop:enable MethodLength
 end
