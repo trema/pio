bac690e14b08f96462241aa744af011ff6abb612
Yasuhito Takamiya <yasuhito@gmail.com>
2015-04-17 13:22:39 +0900

    Renamed lib/pio/flow_mod.rb -> lib/pio/open_flow10/flow_mod.rb

--- lib/pio/open_flow10.rb
+++ lib/pio/open_flow10.rb
@@ -1,5 +1,6 @@
 require 'pio/open_flow10/echo'
 require 'pio/open_flow10/features'
+require 'pio/open_flow10/flow_mod'
 require 'pio/open_flow10/hello'
 require 'pio/open_flow10/packet_in'
 require 'pio/open_flow10/packet_out'
