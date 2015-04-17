c0d5ad7ad0df8ce42ca3abc5e2006667a52d02f0
Yasuhito Takamiya <yasuhito@gmail.com>
2015-04-17 13:07:46 +0900

    Renamed lib/pio/echo.rb -> lib/pio/open_flow10/echo.rb.

--- lib/pio.rb
+++ lib/pio.rb
@@ -2,7 +2,6 @@ require 'pio/parse_error'
 
 require 'pio/arp'
 require 'pio/dhcp'
-require 'pio/echo'
 require 'pio/exact_match'
 require 'pio/features'
 require 'pio/flow_mod'
--- lib/pio/open_flow10.rb
+++ lib/pio/open_flow10.rb
@@ -1,1 +1,2 @@
+require 'pio/open_flow10/echo'
 require 'pio/open_flow10/hello'
