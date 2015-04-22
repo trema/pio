622e0eae4925018a45f2e77a90644650c8f5220a
Yasuhito Takamiya <yasuhito@gmail.com>
2015-04-22 10:53:17 +0900

    Add Pio::OpenFlow::VERSION const.

--- lib/pio/open_flow10.rb
+++ lib/pio/open_flow10.rb
@@ -1,3 +1,10 @@
+module Pio
+  # OpenFlow specific types.
+  module OpenFlow
+    VERSION = 1
+  end
+end
+
 require 'pio/open_flow10/echo'
 require 'pio/open_flow10/exact_match'
 require 'pio/open_flow10/features'
--- lib/pio/open_flow13.rb
+++ lib/pio/open_flow13.rb
@@ -1,3 +1,11 @@
+module Pio
+  # OpenFlow specific types.
+  module OpenFlow
+    remove_const :VERSION
+    VERSION = 4
+  end
+end
+
 require 'pio/open_flow13/echo'
 require 'pio/open_flow13/features_reply'
 require 'pio/open_flow13/features_request'
