2a0bf57d12eee43877e7eea3b8c0373970e43d77
Yasuhito Takamiya <yasuhito@gmail.com>
2015-08-18 10:29:37 +0900

    Refactoring: improve flay score.

--- Rakefile
+++ Rakefile
@@ -1,7 +1,7 @@
 require 'bundler/gem_tasks'
 
 RELISH_PROJECT = 'trema/pio'
-FLAY_THRESHOLD = 526
+FLAY_THRESHOLD = 492
 
 task default: :travis
 task test: [:spec, :cucumber]
--- lib/pio/open_flow13/features_reply.rb
+++ lib/pio/open_flow13/features_reply.rb
@@ -4,43 +4,43 @@ require 'pio/open_flow'
 module Pio
   # OpenFlow 1.3 messages
   module OpenFlow13
-    # OpenFlow 1.3 Features Request and Reply message.
+    # Features Request and Reply message.
     class Features
-      # OpenFlow 1.3 Features Reply message.
+      # Features Reply message.
       class Reply < OpenFlow::Message
-        # OpenFlow 1.3 Features Reply message body.
-        class Body < BinData::Record
-          extend OpenFlow::Flags
-
-          flags_32bit(:capabilities,
-                      [:flow_stats,
-                       :table_stats,
-                       :port_stats,
-                       :group_stats,
-                       :NOT_USED,
-                       :ip_reasm,
-                       :queue_stats,
-                       :NOT_USED,
-                       :port_blocked])
-
-          endian :big
-
-          datapath_id :datapath_id
-          uint32 :n_buffers
-          uint8 :n_tables
-          uint8 :auxiliary_id
-          uint16 :padding
-          hide :padding
-          capabilities :capabilities
-          uint32 :reserved
-
-          def length
-            24
-          end
-        end
-
-        # OpenFlow 1.3 Features Reply message format.
+        # Features Reply message format.
         class Format < BinData::Record
+          # Features Reply message body.
+          class Body < BinData::Record
+            extend OpenFlow::Flags
+
+            flags_32bit(:capabilities,
+                        [:flow_stats,
+                         :table_stats,
+                         :port_stats,
+                         :group_stats,
+                         :NOT_USED,
+                         :ip_reasm,
+                         :queue_stats,
+                         :NOT_USED,
+                         :port_blocked])
+
+            endian :big
+
+            datapath_id :datapath_id
+            uint32 :n_buffers
+            uint8 :n_tables
+            uint8 :auxiliary_id
+            uint16 :padding
+            hide :padding
+            capabilities :capabilities
+            uint32 :reserved
+
+            def length
+              24
+            end
+          end
+
           extend OpenFlow::Format
 
           header version: 4, message_type: 6
