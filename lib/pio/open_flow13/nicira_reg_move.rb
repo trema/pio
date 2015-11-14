cdd99f66e4925f230d573ed7ba41a8ef8fcac89d
Yasuhito Takamiya <yasuhito@gmail.com>
2015-11-14 21:13:56 +0900

    Support reg0-7 as from: and to: in NiciraRegMove

--- lib/pio/open_flow13/nicira_reg_move.rb
+++ lib/pio/open_flow13/nicira_reg_move.rb
@@ -11,11 +11,11 @@ module Pio
       uint16 :n_bits, initial_value: -> { source_oxm_length * 8 }
       uint16 :source_offset, value: 0
       uint16 :destination_offset, value: 0
-      uint16 :source_class, value: Match::OpenFlowBasicValue::OXM_CLASS
+      uint16 :source_oxm_class
       bit7 :source_oxm_field
       bit1 :source_oxm_hasmask, value: 0
       uint8 :source_oxm_length
-      uint16 :destination_oxm_class, value: Match::OpenFlowBasicValue::OXM_CLASS
+      uint16 :destination_oxm_class
       bit7 :destination_oxm_field
       bit1 :destination_oxm_hasmask, value: 0
       uint8 :destination_oxm_length
@@ -23,16 +23,20 @@ module Pio
       attr_reader :from
       attr_reader :to
 
+      # rubocop:disable AbcSize
       def initialize(options)
         @from = options.fetch(:from)
         @to = options.fetch(:to)
         from_klass = Match.const_get(@from.to_s.classify)
         to_klass = Match.const_get(@to.to_s.classify)
-        super(source_oxm_field: from_klass.const_get(:OXM_FIELD),
+        super(source_oxm_class: from_klass.superclass.const_get(:OXM_CLASS),
+              source_oxm_field: from_klass.const_get(:OXM_FIELD),
               source_oxm_length: from_klass.new.length,
+              destination_oxm_class: to_klass.superclass.const_get(:OXM_CLASS),
               destination_oxm_field: to_klass.const_get(:OXM_FIELD),
               destination_oxm_length: to_klass.new.length)
       end
+      # rubocop:enable AbcSize
     end
   end
 end
