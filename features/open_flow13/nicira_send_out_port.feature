a9e48d3bd93fb1b255f37153f7d5de87d91f0b13
Yasuhito Takamiya <yasuhito@gmail.com>
2015-11-19 19:47:54 +0900

    Add some options to NiciraSendOutPort.new

--- features/open_flow13/nicira_send_out_port.feature
+++ features/open_flow13/nicira_send_out_port.feature
@@ -1,6 +1,8 @@
 @open_flow13
 Feature: Pio::NiciraSendOutPort
 
+  Outputs to the OpenFlow port number written to source[offset:offset+n_bits]
+
   Scenario: new(:reg0)
     When I try to create an OpenFlow action with:
       """
@@ -8,8 +10,21 @@ Feature: Pio::NiciraSendOutPort
       """
     Then it should finish successfully
     And the action has the following fields and values:
+      | field             |  value |
+      | offset            |      0 |
+      | n_bits            |     32 |
+      | source            |  :reg0 |
+      | max_length.to_hex | 0xffff |
+
+  Scenario: new(:reg0, offset: 16, n_bits: 16, max_length: 256)
+    When I try to create an OpenFlow action with:
+      """
+      Pio::NiciraSendOutPort.new(:reg0, offset: 16, n_bits: 16, max_length: 256)
+      """
+    Then it should finish successfully
+    And the action has the following fields and values:
       | field      | value |
-      | offset     |     0 |
-      | n_bits     |    32 |
+      | offset     |    16 |
+      | n_bits     |    16 |
       | source     | :reg0 |
-      | max_length |     0 |
+      | max_length |   256 |
--- lib/pio/open_flow13/nicira_send_out_port.rb
+++ lib/pio/open_flow13/nicira_send_out_port.rb
@@ -1,5 +1,6 @@
 require 'pio/open_flow/nicira_action'
 require 'pio/open_flow13/match'
+require 'pio/open_flow13/send_out_port'
 
 module Pio
   module OpenFlow13
@@ -8,7 +9,7 @@ module Pio
       nicira_action_header action_type: 0xffff,
                            action_length: 24,
                            subtype: 15
-      bit10 :_offset, value: 0
+      bit10 :_offset
       bit6 :_n_bits
       struct :_source do
         uint16 :oxm_class
@@ -16,15 +17,17 @@ module Pio
         bit1 :oxm_hasmask, value: 0
         bit8 :oxm_length
       end
-      uint16 :max_length, value: 0
+      uint16 :max_length
       string :zero, length: 6
 
-      def initialize(source)
+      def initialize(source, options = {})
         @source = source
-        super(_n_bits: oxm_length * 8 - 1,
+        super(_n_bits: (options[:n_bits] || oxm_length * 8) - 1,
+              _offset: options[:offset] || 0,
               _source: { oxm_class: source_oxm_class.const_get(:OXM_CLASS),
                          oxm_field: source_oxm_class.const_get(:OXM_FIELD),
-                         oxm_length: oxm_length })
+                         oxm_length: oxm_length },
+              max_length: options[:max_length] || SendOutPort::NO_BUFFER)
       end
 
       attr_reader :source
