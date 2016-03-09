4ad850690780edd6cff4728e26342e65fa68101b
shun159 <shun3382@gmail.com>
2016-03-09 10:31:52 +0900

    Add support for NXAST_CONJUNCTION
    This action provides conjunctive flow match.
    For details, see `ovs-ofctl(8)`.

---
+++ lib/pio/open_flow13/nicira_conjunction.rb
@@ -0,0 +1,25 @@
+require 'pio/open_flow/nicira_action'
+
+module Pio
+  module OpenFlow
+    # NXAST_CONJUNCTION action
+    class NiciraConjunction < OpenFlow::NiciraAction
+      nicira_action_header action_type: 0xffff,
+                           action_length: 16,
+                           subtype: 34
+      uint8 :_clause
+      uint8 :n_clauses
+      uint32 :conjunction_id
+
+      def initialize(options)
+        super(_clause: options[:clause] - 1,
+              n_clauses: options[:n_clauses],
+              conjunction_id: options[:conjunction_id])
+      end
+
+      def clause
+        _clause + 1
+      end
+    end
+  end
+end
