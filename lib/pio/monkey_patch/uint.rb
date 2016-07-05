ed9e88189746298cf7ca01706448a4b1cfea26d3
Yasuhito Takamiya <yasuhito@gmail.com>
2016-07-05 09:22:13 +0900

    Fix error on Ruby 2.0

--- lib/pio/monkey_patch/uint.rb
+++ lib/pio/monkey_patch/uint.rb
@@ -3,7 +3,7 @@ require 'pio/monkey_patch/uint/base_conversions'
 
 # BinData's base module
 module BinData
-  Uint8.include MonkeyPatch::Uint::BaseConversions
-  Uint16be.include MonkeyPatch::Uint::BaseConversions
-  Uint32be.include MonkeyPatch::Uint::BaseConversions
+  Uint8.__send__ :include, MonkeyPatch::Uint::BaseConversions
+  Uint16be.__send__ :include, MonkeyPatch::Uint::BaseConversions
+  Uint32be.__send__ :include, MonkeyPatch::Uint::BaseConversions
 end
